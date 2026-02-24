e49aaa4c8b5ade8dcbb201d97488659f92a5c9d5
ryan3465 <ryan3465@gmail.com>
2026-02-24 18:37:17 +0000

    Update list-component.ts

--- src/app/list-component/list-component.ts
+++ src/app/list-component/list-component.ts
@@ -8,10 +8,12 @@ import { CommonModule } from '@angular/common';
   styleUrl: './list-component.css',
 })
 export class ListComponent {
+  //create string array with president names
 presidents: string[] = ["Higgins", "McAlleses", "Robinson", "Hillery"];
 
+  //create string array with presidents and their terms
 mypresidents: any[] =
-[{name: "Higgins", term:"2011-"},
+[{name: "Higgins", term:"2011-2025"},
 {name: "McAleese", term:"1997-2011"},
 {name: "Robinson", term:"1990-1997"},
 {name: "Hillery", term:"1976-1990"},
